26382fa21f9942e585aba2054dcd9692bde2560d
WEYCHNG <115640987+WEYCHNG@users.noreply.github.com>
2023-11-30 23:20:38 +0800

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -11,13 +11,13 @@ Account::Account()
 	end_date="";
 }
 
-//done
+
 void Account::addAccount() {
 	
 	DBConnection db;
 	
 	db.prepareStatement("Insert into account (UserID,account_name,budget_amount,balance,start_date,end_date) VALUES (?,?,?,?,?,?)");
-	db.stmt->setString(1, UserID);
+	db.stmt->setString(1, UserID);//if your insert 
 	db.stmt->setString(2, account_name);
 	db.stmt->setDouble(3, budget_amount);
 	db.stmt->setDouble(4, balance);
@@ -31,7 +31,7 @@ void Account::addAccount() {
 void Account::update() {
 
 	DBConnection db;
-	db.prepareStatement("UPDATE account SETaccount_name=?, budget_amount=?,balance=?, start_date=?, end_date=? WHERE UserID=?");
+	db.prepareStatement("UPDATE account SET account_name=?, budget_amount=?,balance=?, start_date=?, end_date=? WHERE UserID=?");
 	db.stmt->setString(1, account_name);
 	db.stmt->setDouble(2, budget_amount);
 	db.stmt->setDouble(3, balance);
@@ -45,16 +45,17 @@ void Account::update() {
 
 void Account::removeAccount() {
 	DBConnection db;
-	db.prepareStatement("DELETE FROM account WHERE account_name=?");
-	db.stmt->setString(1, account_name);
+	db.prepareStatement("DELETE FROM account WHERE UserID AND account_name");
+	db.stmt->setString(1, UserID);
+	db.stmt->setString(2, account_name);
 	db.QueryStatement();
 	db.~DBConnection();
 }
 
 Account::Account(sql::ResultSet* data)
 {
-
-	UserID = data->getString("UserID");
+	//AccountID = data->getInt("AccountID");
+	//UserID = data->getString("UserID");
 	account_name = data->getString("account_name");
 	budget_amount = data->getDouble("budget_amount");
 	balance = data->getDouble("balance");
@@ -63,12 +64,11 @@ Account::Account(sql::ResultSet* data)
 }
 
 
-vector<Account> Account::findAccount(string userid, string keyword, string sortColumn, bool ascending) {
+vector<Account> Account::findAccount(string userid,string sortColumn,bool ascending) {
 
 	
 	string query = "SELECT account_name,budget_amount,balance,start_date,end_date FROM `account` WHERE UserID=?"
-	" ORDER BY "+sortColumn;
-	
+		" ORDER BY "+sortColumn;
 
 	if (ascending) {
 		query += " ASC";
@@ -76,12 +76,12 @@ vector<Account> Account::findAccount(string userid, string keyword, string sortC
 	else {
 		query += " DESC";
 	}
-	
-	
+
 	DBConnection db;
 	db.prepareStatement(query);
 	db.stmt->setString(1,userid);
-
+	
+	
 	vector<Account> accounts;
 
 	db.QueryResult();
@@ -99,7 +99,7 @@ vector<Account> Account::findAccount(string userid, string keyword, string sortC
 }
 
 vector<Account> Account::selectAccount(string userid) {
-	string query = "SELECT account_name FROM `account` WHERE UserID=? ";
+	string query = "SELECT account_name,budget_amount,balance,start_date,end_date FROM `account` WHERE UserID=? ";
 
 	DBConnection db;
 	db.prepareStatement(query);
@@ -125,14 +125,13 @@ vector<Account> Account::selectAccount(string userid) {
 bool Account::confirmtoEdit() {
 
 	DBConnection db;
-	db.prepareStatement("SELECT account_name,budget_amount,balance,start_date,end_date FROM account WHERE UserID=? AND account_name=? ");
-	db.stmt->setString(1, UserID);
-	db.stmt->setString(2, account_name);
+	db.prepareStatement("SELECT account_name,budget_amount,balance,start_date,end_date FROM account WHERE account_name=? ");
+	//db.stmt->setString(1, UserID);
+	db.stmt->setString(1, account_name);
 	db.QueryResult();
 	if (db.res->rowsCount() == 1)
 	{
 		while (db.res->next()) {
-			UserID = db.res->getString("UserID");
 			account_name = db.res->getString("account_name");
 			balance = db.res->getDouble("balance");
 			budget_amount = db.res->getDouble("budget_amount");
